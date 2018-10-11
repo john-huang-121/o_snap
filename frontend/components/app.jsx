c8b0cd80bfe726bf5412c315aa33f439b45282c3
john <johnhuang121@gmail.com>
2018-10-11 09:35:26 -0700

    testing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
+// import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
 import SignupContainer from './user/signup_container';
-import PhotoIndexContainer from './photo/photo_index_container';
-import LoginContainer from './session/login_container';
 
 const App = () => (
   <div>
-    <ProtectedRoute path='/photos' component={PhotoIndexContainer} />
-    <AuthRoute path='/signup' component={SignupContainer} />
-    <AuthRoute path='/login' component={LoginContainer} />
+    <Route path='/signup' component={SignupContainer} />
   </div>
 );
 
