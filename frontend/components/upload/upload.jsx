f04b4893dcb363e0c26af45a1af75f0e23ad85f5
john <johnhuang121@gmail.com>
2018-11-15 21:09:08 -0800

    Added image preview when uploading pictures

--- frontend/components/upload/upload.jsx
+++ frontend/components/upload/upload.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Dropzone from 'react-dropzone'; //not used
 
 class Upload extends React.Component {
   constructor(props) {
@@ -9,19 +10,55 @@ class Upload extends React.Component {
       description: '',
       user_id: this.props.currentUser,
       pictureFile: null,
+      pictureUrl: null
     };
 
 
     this.handleInput = this.handleInput.bind(this);
     this.handleFile = this.handleFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleDrop = this.handleDrop.bind(this);
   }
+
+  // componentDidMount() {
+  //   let dropArea = document.getElementById("upload-container");
+
+  //   function addHighlight(e) {
+  //     dropArea.classList.add('highlight');
+  //   }
+
+  //   function removeHighlight(e) {
+  //     dropArea.classList.remove('highlight');
+  //   }
+    
+  //   function preventDefaults(e) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //   }
+
+  //   dropArea.addEventListener('dragenter', addHighlight);
+  //   dropArea.addEventListener("dragover", addHighlight);
+  //   dropArea.addEventListener('dragleave', removeHighlight);
+  //   dropArea.addEventListener('drop', removeHighlight);
+  //   dropArea.addEventListener("drop", this.handleDrop);
+
+  //   ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  //     dropArea.addEventListener(eventName, preventDefaults);
+  //   });
+  // }
 
   handleInput(type) {
     return (e) => {
       this.setState({[type]: e.target.value});
     };
   }
+
+  // handleDrop(e) {
+  //   let dt = e.dataTransfer;
+  //   let files = dt.files;
+  //   this.setState({pictureFile: files});
+  //   console.log(this.state);
+  // }
 
   handleSubmit(e) {
     e.preventDefault();
@@ -35,8 +72,48 @@ class Upload extends React.Component {
   }
 
   handleFile(e) {
-    this.setState({pictureFile: e.target.files[0]});
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      this.setState({ pictureFile: file , pictureUrl: fileReader.result});
+    };
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
   }
+  
+  render() {
+    const preview = this.state.pictureUrl ? <img src={this.state.pictureUrl} /> : null;
+    return (
+      <div className="upload-container" id='upload-container'>
+        <form className="upload-form-container" onSubmit={this.handleSubmit}>
+          <label className="upload-form-label-title">
+            Title
+            <input type="text" className="upload-form-title" onChange={this.handleInput("title")} />
+          </label>
+
+          <label className="upload-form-label-description">
+            description
+            <input type="text" className="upload-form-description" onChange={this.handleInput("description")} />
+          </label>
+
+          <label className="upload-form-label-pictureUrl">
+            pictureUrl
+            <input type="file" className="upload-form-pictureUrl" onChange={this.handleFile} />
+          </label>
+          <h1>Image Preview</h1>
+          {preview}
+          <label className="upload-form-label-submit">
+            <input type="submit" className="upload-form-" value="submit" />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Upload;
 
   // renderErrors() {
   //   if (this.props.errors) {
@@ -49,33 +126,3 @@ class Upload extends React.Component {
   //     );
   //   }
   // }
-
-  render() {
-    return (
-      <div className='upload-container'>
-        <form className='upload-form-container' onSubmit={this.handleSubmit}>
-          <label className='upload-form-label-title'>Title
-            <input type='text' className='upload-form-title'
-              onChange={this.handleInput('title')} />
-          </label>
-
-          <label className='upload-form-label-description'>description
-            <input type='text' className='upload-form-description'
-              onChange={this.handleInput('description')} />
-          </label>
-
-          <label className='upload-form-label-pictureUrl'>pictureUrl
-            <input type='file' className='upload-form-pictureUrl'
-              onChange={this.handleFile}/>
-          </label>
-
-          <label className='upload-form-label-submit'>
-            <input type='submit' className='upload-form-' value='submit' />
-          </label>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default Upload;
