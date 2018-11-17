5527288b1b753303f3ca01b70410e512823344a3
john <johnhuang121@gmail.com>
2018-11-16 20:53:34 -0800

    Code out and style profile skeleton

--- frontend/components/user/profile/profile.jsx
+++ frontend/components/user/profile/profile.jsx
@@ -22,17 +22,19 @@ class Profile extends React.Component {
           </ul>
         </div>
       </div>
-      <h1>Name</h1>
-      <div className='profile-details'>
-        <ul className='profile-details-container'>
+      <div className='profile-details-container'>
+        <div className='profile-username-container'>
+          <h1 className='profile-username'>Name</h1>
+        </div> 
+        <ul className='profile-details'>
           <li># Affection</li>
           <li># Photo Views</li>
           <li># Followers</li>
           <li># Following</li>
         </ul>
       </div>
-      <div className='profile-tabs'>
-        <ul className='profile-tabs-container'>
+      <div className='profile-tabs-container'>
+        <ul className='profile-tabs'>
           <li>PHOTOS</li>
           <li>GALLERIES</li>
           <li>GROUPS</li>
@@ -40,6 +42,14 @@ class Profile extends React.Component {
         </ul>
       </div>
       <div className='profile-grid'>
+        <div className='profile-null'>
+          <img className='profile-null-image' />
+          <ul className='profile-null-content'>
+            <li>You haven't uploaded any photos yet.</li>
+            <li>Get the most out of 500px by uploading your photos and be seen by our global community.</li>
+          </ul>
+          <button>Upload now</button> 
+        </div> 
       </div>
     </div>
     );
