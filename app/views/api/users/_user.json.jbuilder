691ca9bd453c7bb52f1055b0c8a04e88dfe88502
john <johnhuang121@gmail.com>
2019-03-30 00:13:06 -0700

    create true user feed through refactor

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -2,28 +2,28 @@ json.extract! user, :id, :email
 json.follows do
   if all_users
     all_users.each do |empty_user_follow|
-      json.set! empty_user_follow.id, :false
+      json.set! empty_user_follow.id, false
     end
     user.follows.each do |follow|
-      json.set! follow.user_id, :true
+      json.set! follow.user_id, true
     end
   else
     user.follows.each do |follow|
-      json.set! follow.user_id, :true
+      json.set! follow.user_id, true
     end
   end
 end
 json.followers do
   if all_users
     all_users.each do |empty_user_follower|
-      json.set! empty_user_follower.id, :false
+      json.set! empty_user_follower.id, false
     end
     user.followers.each do |follower|
-      json.set! follower.follower_id, :true
+      json.set! follower.follower_id, true
     end
   else
     user.followers.each do |follower|
-      json.set! follower.follower_id, :true
+      json.set! follower.follower_id, true
     end
   end
 end
--- frontend/components/picture/picture_index.jsx
+++ frontend/components/picture/picture_index.jsx
@@ -8,45 +8,57 @@ class PictureIndex extends React.Component {
     this.state = {
       pictures: null,
     }
-    // this.checkFollowed = this.checkFollowed.bind(this);
-    this.allPictures = this.allPictures.bind(this);
+
+    this.currentUserFeed = this.currentUserFeed.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props !== nextProps) {
-      this.allPictures();
+    if (Object.keys(this.props.users).length > 0) {
+      this.currentUserFeed();
     }
   }
 
-  allPictures() {
-    let allPictures = Object.values(this.props.pictures).map(picture => {
-      if (picture.user_id === 1) {
-        return (
-          <PictureIndexItem key={picture.id} picture={picture}
-            user={this.props.users[1]}
-            deletePicture={this.props.deletePicture}
-           />
-        );
-      }
-      else if (picture.user_id == null) { //this line ensures that the recently uploaded pic gets shown
-        return (
-          <PictureIndexUserItem key={picture.id} picture={picture}
-            user={this.props.users[this.props.currentUser]}
-            deletePicture={this.props.deletePicture}
-           />
-        );
-      }
-      else {
-        return (
-          <PictureIndexUserItem key={picture.id} picture={picture}
-            user={this.props.users[picture.user_id]}
-            deletePicture={this.props.deletePicture}
-           />
-        );
+  currentUserFeed() {
+    let currentUserFollows = Object.entries(this.props.users[this.props.currentUser].follows);
+    let userFollowedArr = [];
+    let cleanCurrentUserFollowedPictures;
+    let currentUserFollowed = currentUserFollows.forEach((follow) => {
+      if (follow[1] === true) {
+        userFollowedArr.push(Number(follow[0]));
       }
     });
+    let dirtyCurrentUserFollowedPictures = Object.values(this.props.pictures).map((picture) => {
+      if (userFollowedArr.includes(picture.user_id)) {
+        if (picture.user_id === 1) {
+          return (
+            <PictureIndexItem key={picture.id} picture={picture}
+            user={this.props.users[1]}
+            deletePicture={this.props.deletePicture}
+            />
+          );
+        }
+        else if (picture.user_id == null) { //this line ensures that the recently uploaded pic gets shown
+          return (
+            <PictureIndexUserItem key={picture.id} picture={picture}
+            user={this.props.users[this.props.currentUser]}
+            deletePicture={this.props.deletePicture}
+            />
+          );
+        }
+        else {
+          return (
+            <PictureIndexUserItem key={picture.id} picture={picture}
+            user={this.props.users[picture.user_id]}
+            deletePicture={this.props.deletePicture}
+            />
+          );
+        }
+      }
+      });
+      
+    cleanCurrentUserFollowedPictures = dirtyCurrentUserFollowedPictures.filter((el) => el);
 
-    this.setState({ pictures: allPictures });
+    this.setState({ pictures: cleanCurrentUserFollowedPictures });
   }
 
   render() {
