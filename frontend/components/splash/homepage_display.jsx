45f3f77cffbef1718965b8aa50ab7784d2bd1d11
john <johnhuang121@gmail.com>
2019-04-10 15:11:31 -0700

    reseed local db and now make renderable picture tabs on front page

--- frontend/components/splash/homepage_display.jsx
+++ frontend/components/splash/homepage_display.jsx
@@ -13,6 +13,7 @@ class HomepageDisplay extends React.Component {
     }
 
     this.topPhotosClick = this.topPhotosClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentWillMount() {
@@ -26,72 +27,76 @@ class HomepageDisplay extends React.Component {
     if (this.state.whichTab === 'people') {
       return (
         <div className='homepage-top-photos-grid-container' id='unlog-people'>
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[1].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[2].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[3].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[4].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[5].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[6].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[7].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[17].pictureUrl} />
+        </div>
+      );
+    } else if (this.state.whichTab === 'landscapes') {
+      return (
+        <div className='homepage-top-photos-grid-container' id='unlog-landscapes'>
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[10].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[26].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[28].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[29].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[31].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[36].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[37].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[38].pictureUrl} />
+        </div>
+      );
+    } else if (this.state.whichTab === 'nature') {
+      return (
+        <div className='homepage-top-photos-grid-container' id='unlog-nature'>
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[8].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[9].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[10].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[12].pictureUrl} />
           <img className='homepage-top-photos-grid-item' src={this.props.pictures[13].pictureUrl} />
-          <img className='homepage-top-photos-grid-item' src={this.props.pictures[7].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[15].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[17].pictureUrl} />
+          <img className='homepage-top-photos-grid-item' src={this.props.pictures[19].pictureUrl} />
+        </div>
+      );
+    } else if (this.state.whichTab === 'city') {
+      return (
+        <div className='homepage-top-photos-grid-container' id='unlog-city'>
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[11].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[14].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[21].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[27].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[28].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[34].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[49].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[51].pictureUrl} />
+        </div>
+      );
+    } else if (this.state.whichTab === 'animals') {
+      return (
+        <div className='homepage-top-photos-grid-container' id='unlog-animals'>
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[9].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[16].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[19].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[22].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[23].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[24].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[25].pictureUrl} />
+          <img className='homepage-top-photos-grid-people' src={this.props.pictures[30].pictureUrl} />
         </div>
       );
     }
   }
-  //   } else if (this.state.whichTab === 'landscapes') {
-  //     return (
-  //       <div className='homepage-top-photos-grid-container' id='unlog-landscapes'>
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[1].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[3].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[6].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[10].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[16].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[22].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[23].pictureUrl} />
-  //         <img className='homepage-top-photos-grid-item' src={this.props.pictures[24].pictureUrl} />
-  //       </div>
-  //     );
-  //   } else if (this.state.whichTab === 'nature') {
-  //     return (
-  //       <div className='homepage-top-photos-grid-container' id='unlog-nature'>
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //         <img className='homepage-top-photos-grid-item' src={} />
-  //       </div>
-  //     );
-  //   } else if (this.state.whichTab === 'city') {
-  //     return (
-  //       <div className='homepage-top-photos-grid-container' id='unlog-city'>
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //       </div>
-  //     );
-  //   } else if (this.state.whichTab === 'animals') {
-  //     return (
-  //       <div className='homepage-top-photos-grid-container' id='unlog-animals'>
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //         <img className='homepage-top-photos-grid-people' src={} />
-  //       </div>
-  //     );
-  //   }
-  // }
+
+  handleClick(type) {
+    return (e) => {
+      this.setState({ whichTab: type });
+    };
+  }
 
   isLoggedIn() {
     if (this.props.session === null) {
@@ -117,11 +122,11 @@ class HomepageDisplay extends React.Component {
               <p>Discover what's trending according to photographers around the world.</p>
             </div>
             <ul className='homepage-top-photos-header-list'>
-              <li>People</li>
-              <li>Landscapes</li>
-              <li>Nature</li>
-              <li>City</li>
-              <li>Animals</li>
+              <button onClick={this.handleClick('people')}>People</button>
+              <button onClick={this.handleClick('landscapes')}>Landscapes</button>
+              <button onClick={this.handleClick('nature')}>Nature</button>
+              <button onClick={this.handleClick('city')}>City</button>
+              <button onClick={this.handleClick('animals')}>Animals</button>
             </ul>
           </div>
           {Object.keys(this.props.pictures).length > 0 ? this.topPhotosClick() : null}
