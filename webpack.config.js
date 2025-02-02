f663b2c8f6f60157f6b4dcf6484d855f78100c4d
John H. <johnhuang121@gmail.com>
2025-02-01 21:32:59 -0800

    update to latest node, npm, and packages

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         },
       }
