71ff81375ca9cf3aeba890f11ee865474843f027
John H. <johnhuang121@gmail.com>
2025-02-08 01:35:08 -0800

    refactor upload component

--- frontend/components/upload/upload.jsx
+++ frontend/components/upload/upload.jsx
@@ -1,151 +1,104 @@
-import React from 'react';
-import NavbarContainer from "../navbar/navbar_container";
-import { Link } from 'react-router-dom';
-import Dropzone from 'react-dropzone'; //not used
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NavbarContainer from '../navbar/navbar_container';
 
-class Upload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      description: '',
-      user_id: this.props.currentUser,
-      tags: [],
-      pictureFile: null,
-      pictureUrl: null
-    };
+const Upload = (props) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState([]);
+  const [pictureFile, setPictureFile] = useState(null);
+  const [pictureUrl, setPictureUrl] = useState(null);
 
-    this.handleInput = this.handleInput.bind(this);
-    this.handleFile = this.handleFile.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    // this.handleDrop = this.handleDrop.bind(this);
-  }
+  const navigate = useNavigate();
 
-  // componentDidMount() {
-  //   let dropArea = document.getElementById("upload-container");
-
-  //   function addHighlight(e) {
-  //     dropArea.classList.add('highlight');
-  //   }
-
-  //   function removeHighlight(e) {
-  //     dropArea.classList.remove('highlight');
-  //   }
-    
-  //   function preventDefaults(e) {
-  //     e.preventDefault();
-  //     e.stopPropagation();
-  //   }
-
-  //   dropArea.addEventListener('dragenter', addHighlight);
-  //   dropArea.addEventListener("dragover", addHighlight);
-  //   dropArea.addEventListener('dragleave', removeHighlight);
-  //   dropArea.addEventListener('drop', removeHighlight);
-  //   dropArea.addEventListener("drop", this.handleDrop);
-
-  //   ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  //     dropArea.addEventListener(eventName, preventDefaults);
-  //   });
-  // }
-
-  handleInput(type) {
+  const handleInput = (type) => (e) => {
     if (type === 'tags') {
-      return (e) => {
-        this.setState({ [type]: e.target.value.split(' ') });
-      };
-    } else {
-      return (e) => {
-        this.setState({[type]: e.target.value});
-      };
+      setTags(e.target.value.split(' '));
+    } else if (type === 'title') {
+      setTitle(e.target.value);
+    } else if (type === 'description') {
+      setDescription(e.target.value);
     }
-  }
+  };
 
-  // handleDrop(e) {
-  //   let dt = e.dataTransfer;
-  //   let files = dt.files;
-  //   this.setState({pictureFile: files}); s
-  //   console.log(this.state);
-  // }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    const formData = new FormData();
-    let { title, description, user_id, pictureFile, tags } = this.state;
-
-    formData.append('picture[title]', title);
-    formData.append('picture[description]', description);
-    formData.append('picture[user_id]', user_id);
-    formData.append('picture[picture]', pictureFile);
-    for (let i = 0; i < tags.length; i++) {
-      formData.append('picture[tags][]', tags[i]);
-    }
-    this.props.uploadPicture(formData)
-      .then(() => this.props.history.push('/'));
-  }
-
-  handleFile(e) {
+  const handleFile = (e) => {
     const file = e.target.files[0];
-    const fileReader = new FileReader();
-    fileReader.onloadend = () => {
-      this.setState({ pictureFile: file , pictureUrl: fileReader.result});
-    };
-
     if (file) {
+      const fileReader = new FileReader();
+      fileReader.onloadend = () => {
+        setPictureFile(file);
+        setPictureUrl(fileReader.result);
+      };
       fileReader.readAsDataURL(file);
     }
-  }
-  
-  render() {
-    const preview = this.state.pictureUrl ? <img src={this.state.pictureUrl} /> : null;
-    return (
-      <div className="upload-container" id='upload-container'>
-        <NavbarContainer />
-        <form className="upload-form-container" onSubmit={this.handleSubmit}>
-          <label className="upload-form-label-title" htmlFor="upload-title">
-            Title:
-          </label>
-          <input type="text" className="upload-form-title" id="upload-title" onChange={this.handleInput("title")} />
-          <br></br>
-          <label className="upload-form-label-description" htmlFor="upload-desc">
-            Description:
-          </label>
-          <textarea 
-            className="upload-form-description"
-            id="upload-desc"
-            wrap="soft"
-            onChange={this.handleInput("description")} />
-          <br></br>
-          <label className="upload-form-label-tags" htmlFor="upload-tags">
-            Tags (space separated):
-          </label>
-          <input type="text" className="upload-form-tags" id="upload-tags" onChange={this.handleInput("tags")} />
-          <br></br>
-          <label className="upload-form-label-pictureUrl" htmlFor="upload-img">
-            Image:
-          </label>
-          <input type="file" className="upload-form-pictureUrl" id="upload-img" onChange={this.handleFile} />
-          {preview}
-          <br></br>
-          <label className="upload-form-label-submit">
-            <input type="submit" className="upload-form" value="submit" />
-          </label>
-        </form>
-      </div>
-    );
-  }
-}
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('picture[title]', title);
+    formData.append('picture[description]', description);
+    formData.append('picture[user_id]', props.currentUser);
+    formData.append('picture[picture]', pictureFile);
+    tags.forEach((tag) => {
+      formData.append('picture[tags][]', tag);
+    });
+    props.uploadPicture(formData).then(() => navigate('/'));
+  };
+
+  const preview = pictureUrl ? <img src={pictureUrl} alt="Preview" /> : null;
+
+  return (
+    <div className="upload-container" id="upload-container">
+      <NavbarContainer />
+      <form className="upload-form-container" onSubmit={handleSubmit}>
+        <label className="upload-form-label-title" htmlFor="upload-title">
+          Title:
+        </label>
+        <input
+          type="text"
+          className="upload-form-title"
+          id="upload-title"
+          onChange={handleInput('title')}
+        />
+        <br />
+        <label className="upload-form-label-description" htmlFor="upload-desc">
+          Description:
+        </label>
+        <textarea
+          className="upload-form-description"
+          id="upload-desc"
+          wrap="soft"
+          onChange={handleInput('description')}
+        />
+        <br />
+        <label className="upload-form-label-tags" htmlFor="upload-tags">
+          Tags (space separated):
+        </label>
+        <input
+          type="text"
+          className="upload-form-tags"
+          id="upload-tags"
+          onChange={handleInput('tags')}
+        />
+        <br />
+        <label className="upload-form-label-pictureUrl" htmlFor="upload-img">
+          Image:
+        </label>
+        <input
+          type="file"
+          className="upload-form-pictureUrl"
+          id="upload-img"
+          onChange={handleFile}
+        />
+        {preview}
+        <br />
+        <label className="upload-form-label-submit">
+          <input type="submit" className="upload-form" value="submit" />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default Upload;
-
-  // renderErrors() {
-  //   if (this.props.errors) {
-  //     return (
-  //       <ul className='upload-errors-container'>
-  //         {this.props.errors.map((error) => (
-  //           <li className='upload-error' key={error.id}>{error}</li>)
-  //         )}
-  //       </ul>
-  //     );
-  //   }
-  // }
