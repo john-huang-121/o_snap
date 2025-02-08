f82c0283621476321a2aa5e07b66de8f70fd2e8d
John H. <johnhuang121@gmail.com>
2025-02-07 22:49:29 -0800

    refactor modael

--- frontend/components/modal/picture_modal.jsx
+++ frontend/components/modal/picture_modal.jsx
@@ -5,84 +5,63 @@ import CommentIndex from '../comment/comment_index_container';
 
 const Timestamp = require("react-timestamp");
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Modal = ({ modalOpen, closeModal, picture, users }) => {
+  let modalClass = modalOpen ? 'modal-open' : 'modal-closed';
 
-  render() {
-    let modalClass = this.props.modalOpen ? 'modal-open' : 'modal-closed';
-
-    return (
-      <div className={`${modalClass}`}>
-        <section className='modal-window'>
-          <div className='modal-picture'>
-            <img src={this.props.picture.pictureUrl} />
-          </div>
-          <div className='modal-profile'>
-            <div className='modal-author-info-container'>
-              <div className='modal-author-info'>
-                <div className='modal-author-icon'>
-                  <img src={this.props.users[this.props.picture.user_id].profile_pic} />
-                </div>
-                <div className='modal-author-email-timestamp-container'>
-                  <Link to={`/user/${this.props.picture.user_id}`}>{this.props.users[this.props.picture.user_id].email}</Link>
-                  <Timestamp className='profile-gallery-timestamp'
-                    time={this.props.picture.created_at} format='ago' />
-                </div>
+  return (
+    <div className={`${modalClass}`}>
+      <section className='modal-window'>
+        <div className='modal-picture'>
+          <img src={picture.pictureUrl} />
+        </div>
+        <div className='modal-profile'>
+          <div className='modal-author-info-container'>
+            <div className='modal-author-info'>
+              <div className='modal-author-icon'>
+                <img src={users[picture.user_id].profile_pic} />
               </div>
-              <div className="modal-close-button-container">
-                <button className="modal-close-button" onClick={this.props.closeModal}>x</button>
+              <div className='modal-author-email-timestamp-container'>
+                <Link to={`/user/${picture.user_id}`}>{users[picture.user_id].email}</Link>
+                <Timestamp className='profile-gallery-timestamp'
+                  time={picture.created_at} format='ago' />
               </div>
             </div>
-            {/* <div href='#' className='picture-index-item-user-header-container'>
-              <img className='picture-index-item-user-profile-pic' src={user.profile_pic} />
-              <div className='picture-index-item-email-timestamp-container'>
-              </div>
-            </div> */}
-            <div className='modal-description-and-icon-container'>
-              <div className=''>
-                {/* <h1>title</h1>
-                <p>description</p> */}
-                <div className='modal-title'>
-                  {this.props.picture.title}
-                </div>
-                <p className='modal-desc'>
-                  {this.props.picture.description}
-                </p>
-              </div>
-
-              <div className='modal-icon-container'>
-                <a href='#' className='modal-icons'>
-                  <li className='picture-index-item-user-messenger-icon' />
-                </a>
-
-                <a href='#' className='modal-icons'>
-                  <li className='picture-index-item-user-comment-icon' />
-                </a>
-
-                <a href='#' className='modal-icons'>
-                  <li className='picture-index-item-user-gallery-icon' />
-                </a>
-
-                <a href='#' className='modal-icons'>
-                  <li className='picture-index-item-user-gallery-icon' />
-                </a>
-
-                <LikeContainer picture={this.props.picture} />
-              </div>
+            <div className="modal-close-button-container">
+              <button className="modal-close-button" onClick={closeModal}>x</button>
             </div>
           </div>
-
-          <div className='modal-comment'>
-            <CommentIndex picture={this.props.picture} />
+          <div className='modal-description-and-icon-container'>
+            <div className=''>
+              <div className='modal-title'>
+                {picture.title}
+              </div>
+              <p className='modal-desc'>
+                {picture.description}
+              </p>
+            </div>
+            <div className='modal-icon-container'>
+              <a href='#' className='modal-icons'>
+                <li className='picture-index-item-user-messenger-icon' />
+              </a>
+              <a href='#' className='modal-icons'>
+                <li className='picture-index-item-user-comment-icon' />
+              </a>
+              <a href='#' className='modal-icons'>
+                <li className='picture-index-item-user-gallery-icon' />
+              </a>
+              <a href='#' className='modal-icons'>
+                <li className='picture-index-item-user-gallery-icon' />
+              </a>
+              <LikeContainer picture={picture} />
+            </div>
           </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+        <div className='modal-comment'>
+          <CommentIndex picture={picture} />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Modal;
-
-// https://alligator.io/react/modal-component/
