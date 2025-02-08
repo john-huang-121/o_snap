0474ee0e7ac68b10e2f96301ea5d8e47059d3a10
John H. <johnhuang121@gmail.com>
2025-02-08 01:19:36 -0800

    refactor login component

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -1,117 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import NavbarContainer from '../navbar/navbar_container';
 import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  useEffect(() => {
+    props.clearAllErrors();
+  }, [props.clearAllErrors]);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
 
-  componentDidMount() {
-    this.props.clearAllErrors();
-  }
-
-  handleInput(type) {
-    return (e) => {
-      this.setState({[type]: e.target.value});
-    };
-  }
-
-  demoLogin(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState(
-      {email: 'demo_user@gmail.com', password: '123456'}, () => {
-      this.props.login(this.state)
-        .then(() => this.props.history.push('/'));
-    });
-  }
+    props.login({ email, password })
+      .then(() => navigate('/'));
+  };
 
-  handleSubmit(e) {
+  const demoLogin = (e) => {
     e.preventDefault();
-    this.props.login(this.state)
-      .then(() => this.props.history.push('/'));
-  }
+    const demoEmail = 'demo_user@gmail.com';
+    const demoPassword = '123456';
+    // Directly use demo credentials without waiting for state update.
+    props.login({ email: demoEmail, password: demoPassword })
+      .then(() => navigate('/'));
+  };
 
-  renderErrors() {
-    if (this.props.errors) {
+  const renderErrors = () => {
+    if (props.errors && props.errors.length > 0) {
       return (
-        <ul className='login-errors-container'>
-          {this.props.errors.map((error) => (
-            <li className='login-error' key={error.id}>{error}</li>)
-          )}
+        <ul className="login-errors-container">
+          {props.errors.map((error, idx) => (
+            <li className="login-error" key={idx}>{error}</li>
+          ))}
         </ul>
       );
     }
-  }
+    return null;
+  };
 
-  render () {
-    return <section className="login-page-container">
-        <NavbarContainer />
-        <div className="login-page-after-nav">
-          <div className="login-header-flex-container">
-            <div className="login-header">
-              <ul className="login-header-texts">
-                <li>Log In to 500px</li>
-              </ul>
-            </div>
+  return (
+    <section className="login-page-container">
+      <NavbarContainer />
+      <div className="login-page-after-nav">
+        <div className="login-header-flex-container">
+          <div className="login-header">
+            <ul className="login-header-texts">
+              <li>Log In to 500px</li>
+            </ul>
+          </div>
 
-            <div className="login-form-container">
-              <form className="login-form" onSubmit={this.handleSubmit}>
-                <label className="login-form-email-username-container">
-                  Email or Username
-                  <input className="login-input" type="email" value={this.state.email} onChange={this.handleInput("email")} />
-                </label>
+          <div className="login-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <label className="login-form-email-username-container">
+                Email or Username
+                <input 
+                  className="login-input" 
+                  type="email" 
+                  value={email} 
+                  onChange={handleEmailChange} 
+                />
+              </label>
 
-                <label className="login-form-password-container">
-                  <div className="login-form-password-header">
-                    Password<a href="#login" className="forgot-pass">
-                      Forgot password?
-                    </a>
-                  </div>
-                  <input className="login-input" type="password" value={this.state.password} onChange={this.handleInput("password")} />
-                </label>
-
-                {this.renderErrors()}
-
-                <div className="login-forms-container">
-                  <input type="submit" value="Log in" />
-                  <button onClick={e => this.demoLogin(e)}>
-                    Demo Log In
-                  </button>
+              <label className="login-form-password-container">
+                <div className="login-form-password-header">
+                  Password
+                  <a href="#login" className="forgot-pass">
+                    Forgot password?
+                  </a>
                 </div>
+                <input 
+                  className="login-input" 
+                  type="password" 
+                  value={password} 
+                  onChange={handlePasswordChange} 
+                />
+              </label>
 
-                <div className="login-buttons">
-                  <button className="login-facebook-button" onClick={e => this.demoLogin(e)}>
-                    <div className="login-facebook-icon" />
-                    <p>Continue with Facebook</p>
-                  </button>
+              {renderErrors()}
 
-                <button className="login-google-button" onClick={e => this.demoLogin(e)}>
-                    <div className="login-google-icon" />
-                    <p>Continue with Google</p>
-                  </button>
-                </div>
-              </form>
-            </div>
+              <div className="login-forms-container">
+                <input type="submit" value="Log in" />
+                <button onClick={demoLogin}>
+                  Demo Log In
+                </button>
+              </div>
 
-            <div className="login-page-link-container">
-              <li className="login-page-link">
-                Don't have an account?&nbsp;
-                <a href="#signup">
-                  Sign up
-                </a>
-              </li>
-            </div>
+              <div className="login-buttons">
+                <button className="login-facebook-button" onClick={demoLogin}>
+                  <div className="login-facebook-icon" />
+                  <p>Continue with Facebook</p>
+                </button>
+                <button className="login-google-button" onClick={demoLogin}>
+                  <div className="login-google-icon" />
+                  <p>Continue with Google</p>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="login-page-link-container">
+            <li className="login-page-link">
+              Don't have an account?&nbsp;
+              <Link to="/signup">
+                Sign up
+              </Link>
+            </li>
           </div>
         </div>
-      </section>;
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default Login;
