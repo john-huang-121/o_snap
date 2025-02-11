66bf02efc06e4b31b3157d6d4130c5c258f0276e
John H. <johnhuang121@gmail.com>
2025-02-10 23:12:41 -0800

    move react-refresh into webpack dev portion

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,11 +1,12 @@
 // // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig, inliningCss } = require('shakapacker');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const webpackConfig = generateWebpackConfig();
 
 if (isDevelopment && inliningCss) {
+  const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
   webpackConfig.plugins.push(
     new ReactRefreshWebpackPlugin({
       overlay: {
