5850ed4906229b514a7341d5e13bd2f0dafc6769
John H. <johnhuang121@gmail.com>
2025-02-10 23:40:08 -0800

    remove unnecessary packages and configs

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,19 +1,6 @@
 // // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig, inliningCss } = require('shakapacker');
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const { generateWebpackConfig } = require('shakapacker')
 
-const webpackConfig = generateWebpackConfig();
+const webpackConfig = generateWebpackConfig()
 
-if (isDevelopment && inliningCss) {
-  const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
-  webpackConfig.plugins.push(
-    new ReactRefreshWebpackPlugin({
-      overlay: {
-        sockPort: webpackConfig.devServer.port,
-      },
-    })
-  );
-}
-
-module.exports = webpackConfig;
+module.exports = webpackConfig
